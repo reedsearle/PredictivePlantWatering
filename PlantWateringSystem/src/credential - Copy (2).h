6695200b536ffde1778cf6826b2a1e0eae727b6e
Reed Searle <reed_searle@hotmail.com>
2022-03-22 10:24:03 -0600

    remove credentials

--- PlantWateringSystem/src/credential - Copy (2).h
+++
@@ -1,5 +0,0 @@
-/******************* Adafruit.io Setup *****************/
-#define AIO_SERVER         "io.adafruit.com"
-#define AIO_SERVERPORT     1883  // Use 8883 for SSL
-#define AIO_USERNAME       "reedsearle"
-#define AIO_KEY            "aio_pAeh12swBzbHQLWljO9RvwTjWHGI"
